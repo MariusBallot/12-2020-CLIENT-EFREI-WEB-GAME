6f6af9ce3b8cd7fd6636b5172d6f78516b142db5
Adrien Calmesnil <adcalmesnil@gmail.com>
2020-12-26 16:24:29 +0100

    routes setup

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,44 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import DashBoard from '../views/DashBoard.vue'
 import Game from '../views/Game.vue'
+import Login from '../views/Login.vue'
+import LeaderBoard from '../views/LeaderBoard.vue'
+import Rewards from '../views/Rewards.vue'
+import Tutorial from '../views/Tutorial.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'DashBoard',
+    component: DashBoard
   },
   {
     path: '/Game',
     name: 'Game',
     component: Game
+  },
+  {
+    path: '/Login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/LeaderBoard',
+    name: 'LeaderBoard',
+    component: LeaderBoard
+  },
+  {
+    path: '/Rewards',
+    name: 'Rewards',
+    component: Rewards
+  },
+  {
+    path: '/Tutorial',
+    name: 'Tutorial',
+    component: Tutorial
   },
 
 ]
---
+++ src/views/LeaderBoard.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="LeaderBoard"><h1>Le LeaderBoard très très bo !</h1></div>
+</template>
+
+<script>
+export default {
+  name: "LeaderBoard",
+};
+</script>
