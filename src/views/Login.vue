761cb4ba46d90a332d70bb73068e6d0501fff6f0
Adrien Calmesnil <adcalmesnil@gmail.com>
2020-12-27 12:38:10 +0100

    login css+html

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="login"><h1>Connerie de adrien</h1></div>
+  <div class="login">
+    <h2>WELCOME BACK CAPTAIN</h2>
+    <br />
+    <form id="login" method="get">
+      <input type="text" name="Uname" id="Uname" placeholder="Username" />
+      <br /><br />
+
+      <input type="Password" name="Pass" id="Pass" placeholder="Password" />
+      <br /><br />
+
+      <input type="button" name="log" id="log" value="Login" />
+      <br /><br />
+
+      <input type="button" name="create" id="create" value="Create Account" />
+      <br /><br />
+    </form>
+  </div>
 </template>
 
 <script>
@@ -7,4 +23,76 @@ export default {
   name: "Login",
 };
 </script>
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #6abadeba;
+  font-family: 'Arial';
+}
+
+.login {
+  width: 382px;
+  overflow: hidden;
+  margin: auto;
+  margin: 20 0 0 450px;
+  padding: 80px;
+  border-radius: 15px;
+}
+
+h2 {
+  text-align: center;
+  color: red;
+  padding: 20px;
+}
+
+label {
+  color: #08ffd1;
+  font-size: 17px;
+}
+
+#Uname {
+  width: 300px;
+  height: 30px;
+  border: 1px solid #5EF7FF;
+  box-sizing: border-box;
+  border-radius: 10px;
+  padding-left: 8px;
+}
+
+#Pass {
+  width: 300px;
+  height: 30px;
+  border: 1px solid #5EF7FF;
+  box-sizing: border-box;
+  border-radius: 10px;
+  padding-left: 8px;
+}
+
+#log {
+  width: 300px;
+  height: 30px;
+  border: none;
+  border-radius: 17px;
+  padding-left: 7px;
+  color: blue;
+}
+
+#create {
+  width: 300px;
+  height: 30px;
+  border: none;
+  border-radius: 17px;
+  padding-left: 7px;
+  color: blue;
+}
+
+span {
+  color: white;
+  font-size: 17px;
+}
+
+a {
+  float: right;
+}
+</style>
